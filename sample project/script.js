5875b147722ce0f0f5f95860e4f35206806ed2c3
Vishakha <90548410+vishakha1821@users.noreply.github.com>
2022-03-04 14:58:45 +0530

    Add files via upload

--- sample project/script.js
+++ sample project/script.js
@@ -1,11 +1,15 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 
 
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+//creating an event for filter
+filter.addEventListener('keyup', filterItems);
+
 
 
 // Add item
@@ -55,5 +59,25 @@ function removeItem(e){
       itemList.removeChild(li);
     }
   }
+}
+
+//Making a function of filterItems 
+function filterItems(e){
+
+    // convert to lowercase 
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+    //coverting it in the form of array 
+   Array.from(items).forEach(function(item){
+     var itemName = item.firstChild.textContent;
+     if(itemName.toLowerCase().indexOf(text)!=-1){
+       item.style.display = 'block';
+     }else {
+       item.style.display = 'none';
+     }
+   });
+
+      
+    
 }
 
